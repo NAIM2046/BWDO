07049ef61f62ba43873ca456d8748be8b2c578b9
NAIM2046 <nmd98911@gmail.com>
2025-10-26 00:10:38 +0600

    add vision-mission

---
+++ src/app/(root)/vission-mission/page.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      <h1>Vision and Mission</h1>
+      <p>Our vision is to...</p>
+      <p>Our mission is to...</p>
+    </div>
+  );
+};
+
+export default page;
