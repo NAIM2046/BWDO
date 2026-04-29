6278356ba9fb9f0cbc94072e4adcc0eeb3539222
Naim Hossain <nmd98911@gmail.com>
2026-04-29 20:02:21 +0600

    axio problem

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -1,10 +1,13 @@
 import axios from "axios";
 
+const baseURL =
+  process.env.NEXT_PUBLIC_API_URL || "";
+
 export const axiosPublic = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL,
+  baseURL: baseURL,
 });
 
 export const axiosPrivate = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL,
-  withCredentials: true, // cookie auto-send হবে
+  baseURL: baseURL,
+  withCredentials: true,
 });
